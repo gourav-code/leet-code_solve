69104fc917ed5233f144d6fd6cb01276d06b6077
songara <kumarstarsongara@gmail.com>
2024-05-09 13:22:05 +0530

    best DP problem

---
+++ coinChangeDP.py
@@ -0,0 +1,12 @@
+class Solution:
+    def coinChange(self, coins: [int], amount: int) -> int:
+        l = [amount+1]*(amount+1)  #amount +1 is max int for this question we can also use another max int and that from 0 ... amount
+        l[0] = 0
+
+        for i in range(1, amount+1):
+            for tmp in coins:
+                if i-tmp >= 0:
+                    l[i] = min(l[i], 1 + l[i-tmp])
+
+        return l[amount] if l[amount] != amount+1 else -1
+        
