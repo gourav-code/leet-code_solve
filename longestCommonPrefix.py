f3aab375be01750f59eff903a455a1cb2cfc3ff5
songara <kumarstarsongara@gmail.com>
2024-07-07 14:46:04 +0530

    two solution, first being awesome

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -14,3 +14,43 @@ class Solution:
             return '-1'
         
         return ans
+
+#----------------------------------------------------------------------------------------------
+#second best solution
+#User function Template for python3
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endofWord = False
+        
+    def addWord(self, word):
+        cur = self
+        
+        for ch in word:
+            if ch not in cur.children:
+                cur.children[ch] = TrieNode()
+            cur = cur.children[ch]
+            
+        cur.endofWord = True
+        
+#second 
+class Solution2:
+    def longestCommonPrefix(self, n, arr):
+        # code here
+        root = TrieNode()
+        for tmp in arr:
+            root.addWord(tmp)
+            
+        ans = ''
+        i = 0
+        while len(root.children) == 1 and i < len(arr[0]):
+            ans += arr[0][i]
+            root = root.children[arr[0][i]]
+            if root.endofWord:
+                break
+            i += 1
+        
+        if len(ans) == 0:
+            return '-1'
+        
+        return ans
